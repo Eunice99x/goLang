80ead4066cdcdf231e6a416a5202f5a4daf30188
Younes Outerbah <younesouterbah1@gmail.com>
2024-04-04 18:59:05 +0100

    better function to read files

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"younes.dev/go/fileutils"
 )
 
 func main() {
-	print(fileutils.ReadTextFile("data/file.txt"))
+
+	rootDir, _ := os.Getwd()
+
+	c, err :=fileutils.ReadTextFile(rootDir + "/data/file.txt")
+	if err == nil {
+		fmt.Print(c)
+	}else{
+		fmt.Printf("AHHAHAHA %v", err)
+		// panic("AHHAHAHA %v", err)
+	}
 }
