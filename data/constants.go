28e83fdf50df2d1d9a9bf8ce868c8cb20110db92
Younes Outerbah <younesouterbah1@gmail.com>
2024-04-04 17:39:38 +0100

    simple calculator

---
+++ data/constants.go
@@ -0,0 +1,3 @@
+package data
+
+var MaxSpeed int = 120
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
-	print("Hello, World!")
-	newFunction()
+	var operator string
+	var num1, num2 int
+
+	// newFunction()
+	// fmt.Println(data.MaxSpeed)
+	
+	fmt.Println("CALCULATOR GO 1.0")
+	fmt.Println("=================")
+	fmt.Println("which operation u want to preform? (add, substract, multiplay, divide)")
+	fmt.Scanf("%s\n", &operator)
+	fmt.Println("pick first number")
+	fmt.Scanf("%d\n", &num1)
+	fmt.Println("pick second number")
+	fmt.Scanf("%d\n", &num2)
+	switch operator {
+	case "add":
+		fmt.Println( num1 + num2)
+	case "substract":
+		fmt.Println( num1 - num2)
+	case "multiplay":
+		fmt.Println( num1 * num2)
+	case "divide":
+		fmt.Println( num1 / num2)
+	}
 }
