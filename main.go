643c79137bfd2a4da8bf325983ba8b5b74c0892e
Younes Outerbah <younesouterbah1@gmail.com>
2024-04-04 18:51:00 +0100

    reading file content from os package

--- main.go
+++ main.go
@@ -1,33 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"younes.dev/go/fileutils"
 )
 
 func main() {
-
-	var operator string
-	var num1, num2 int
-
-	// newFunction()
-	// fmt.Println(data.MaxSpeed)
-	
-	fmt.Println("CALCULATOR GO 1.0")
-	fmt.Println("=================")
-	fmt.Println("which operation u want to preform? (add, substract, multiplay, divide)")
-	fmt.Scanf("%s\n", &operator)
-	fmt.Println("pick first number")
-	fmt.Scanf("%d\n", &num1)
-	fmt.Println("pick second number")
-	fmt.Scanf("%d\n", &num2)
-	switch operator {
-	case "add":
-		fmt.Println( num1 + num2)
-	case "substract":
-		fmt.Println( num1 - num2)
-	case "multiplay":
-		fmt.Println( num1 * num2)
-	case "divide":
-		fmt.Println( num1 / num2)
-	}
+	print(fileutils.ReadTextFile("data/file.txt"))
 }
