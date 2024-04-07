e778823efc6eb68dfaa0bf9cf186afffd4f0bbf1
Younes Outerbah <younesouterbah1@gmail.com>
2024-04-07 03:10:20 +0100

    cleaned the project to start creating a backend server

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"younes.dev/go/crypto/api"
-)
-
 func main() {
-	rate, err := api.GetRate("BTC")
-	if err != nil {
-		fmt.Printf("The rate for %v is %v \n", rate.Currency, rate.Price)
-	}
+
 }
